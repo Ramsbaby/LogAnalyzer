3063238c7df6176d78d03f234f85a3a5df7bcbd0
ramsbaby <a45979002@gmail.com>
2021-07-21 02:38:15 +0900

    coding

---
+++ src/com/ramsbaby/parser/model/UrlModel.java
@@ -0,0 +1,23 @@
+package com.ramsbaby.parser.model;
+
+import com.ramsbaby.parser.common.type.StatusCode;
+import com.ramsbaby.parser.common.type.WebBrowser;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @author RAMSBABY
+ * @date 2021-07-21 오전 2:33
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UrlModel {
+    private String mainUrl;
+    private String serviceID;
+    private String apiKey;
+    private String apiQuery;
+}
